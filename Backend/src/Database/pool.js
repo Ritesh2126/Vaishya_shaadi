ffa5c26654371873756df9857a16158d6ae9b473
Ritesh2126 <riteshgupta2100@gmail.com>
2024-03-30 00:56:22 +0530

    complete o

--- Backend/src/Database/pool.js
+++ Backend/src/Database/pool.js
@@ -1,23 +1,11 @@
-var mysql=require("mysql2")
+const mysql = require("mysql2");
 
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306, // Optional, defaults to 3306
+    user: 'root',
+    password: 'Rajgupta@1234',
+    database: 'testing'
+});
 
-const pool=mysql.createPool(
-    { host:'localhost',
-    port:3306,
-    user:'root',
-    password:'Rajgupta@1234',
-    database:'testing'
-})
-// const pool=mysql.createPool(
-//     { hostname:'ftp://vaishyasamajshaadi.com',
-//     // port:3306,
-//     user:'u162132502_rajgupta',
-//     password:'2O+$Q93sfNx!',
-//     database:'u162132502_Shaadi'
-// })
-
-
-
-
-
-module.exports=pool
+module.exports = pool;
