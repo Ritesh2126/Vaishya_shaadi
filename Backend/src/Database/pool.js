f29bdc2bc2333f286f7afe36c58407bddc500a9a
Ritesh2126 <70127327+Ritesh2126@users.noreply.github.com>
2024-03-30 01:03:48 +0530

    Update pool.js

--- Backend/src/Database/pool.js
+++ Backend/src/Database/pool.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306, // Optional, defaults to 3306
-    user: 'root',
-    password: 'Rajgupta@1234',
-    database: 'testing'
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     port: 3306, // Optional, defaults to 3306
+//     user: 'root',
+//     password: 'Rajgupta@1234',
+//     database: 'testing'
+// });
 
-module.exports = pool;
+// module.exports = pool;
