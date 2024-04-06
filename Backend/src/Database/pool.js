e6a89e91c24e7b55dc12e80c642354a7b4978c7b
Ritesh2126 <70127327+Ritesh2126@users.noreply.github.com>
2024-04-06 14:05:56 +0530

    Update pool.js

--- Backend/src/Database/pool.js
+++ Backend/src/Database/pool.js
@@ -4,8 +4,8 @@ const pool = mysql.createPool({
     host: 'localhost',
     port: 3306, // Optional, defaults to 3306
     user: 'root',
-    password: 'Rajgupta@1234',
-    database: 'testing'
+    password: '',
+    database: 'shaadi'
 });
 
 module.exports = pool;
